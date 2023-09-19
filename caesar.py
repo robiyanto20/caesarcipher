a36c28285628b860e2f65afff234322ca663a5a4
mas agus kalimantan <38169654+robiyanto20@users.noreply.github.com>
2023-09-19 13:53:59 +0700

    Update caesar.py
    Encrypt And Decrypt PYTHON Caesar Cipher

--- caesar.py
+++ caesar.py
@@ -1,25 +1,55 @@
-def encrypt_text(plaintext,n):
-    ans = ""
-    # iterate over the given text
-    for i in range(len(plaintext)):
-        ch = plaintext[i]
-        
-        # check if space is there then simply add space
-        if ch==" ":
-            ans+=" "
-        # check if a character is uppercase then encrypt it accordingly 
-        elif (ch.isupper()):
-            ans += chr((ord(ch) + n-65) % 26 + 65)
-        # check if a character is lowercase then encrypt it accordingly
-        
-        else:
-            ans += chr((ord(ch) + n-97) % 26 + 97)
-    
-    return ans
+def transform(x): 
+    x = list(x)
+    for i,n in enumerate(x):
+        if n in ABC:
+            x[i] = ABC.index(n)
+    return x
+def crypt(x,key):
+    x = transform(x)
+    x = [(n + key)%26 if type(n) == int else n for i,n in enumerate(x)]
+    return x
 
-plaintext = "ROBIYANTO_312210098"
-n = 1
-print("Plain Text is : " + plaintext)
-print("Shift pattern is : " + str(n))
-print("Cipher Text is : " + encrypt_text(plaintext,n))
+def decrypt(x,key):
+    x = transform(x)
+    d = []
+    if key == None:
+        for k in range(26):
+            d.append(crypt(x,-k))
+        x = d
+        return x
+    else: 
+        return crypt(x,-key)
 
+def caesar(x,key,mod):
+    v = []
+    if mod:
+        x = crypt(x,key)
+    else:
+        x = decrypt(x,key)
+    for i,n in enumerate(x):
+        if type(n) == int:
+            x[i] = ABC[n]
+        elif type(n) == list:
+            for j,m in enumerate(n):
+                if type(m) == int:
+                    n[j] = ABC[m]
+            value = ''.join(n)
+            #v.append('==> key = {} : {}'.format(i,value)) #for old version
+            v.append(f'==> key = {i} : {value}')
+    if len(v) != 0:
+        return '\n'.join(v)
+    #return "==> {}.format(''.join(x))" #for old version
+    return f"==> {''.join(x)}"
+
+ABC = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+if input('==> Encrypt or Decrypt ? : ').lower() == 'encrypt':
+    x = input('Enter what you want to encrypt => ').upper()
+    key = int(input('Enter the key  [0-25] => '))
+    mod = True
+else : 
+    x = input('Enter what you want to decrypt => ').upper()
+    if input('Do you have the key ? (y/n) :').lower() == 'y':
+        key = int(input('Enter the key => '))
+    else: key=None
+    mod = False	
+print(caesar(x,key,mod))
